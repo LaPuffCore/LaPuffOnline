cf1dbc556652101967fd2ca246ddf4e082936835
LaPuffCore <justinlapuff@gmail.com>
2026-04-07 05:36:19 +0000

    fix vite config for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,17 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Set the base path to match your GitHub repository name
+  base: '/LaPuffOnline/',
+  
   logLevel: 'error', // Suppress warnings, only show errors
+  
   plugins: [
     react(),
-  ]
+  ],
+  
+  // Optional: Ensures your build output matches what GitHub Actions expects
+  build: {
+    outDir: 'dist',
+  }
 });
